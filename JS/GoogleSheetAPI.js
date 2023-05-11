eb719e6acaf7b58c86ac144b178f31daf270b59a
PortableUser <sam.yan.kc@mail.com>
2023-05-11 22:29:07 +0800

    .

--- JS/GoogleSheetAPI.js
+++ JS/GoogleSheetAPI.js
@@ -2,8 +2,7 @@ function Read(Sheet, Range) {
     return `this is a sample response from reading ${Sheet}!${Range}`
 }
 
-function Replace(ObjectID) {
-    let ObjectRef = document.getElementById(ObjectID)
+function Replace(ObjectRef) {
     return {
         By: (Content) => {
             ObjectRef.innerText = Content
